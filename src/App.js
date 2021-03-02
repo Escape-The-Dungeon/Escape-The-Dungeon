627b3b9b578ae23afbd99983c2180153f38bc11f
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-03-02 14:24:56 -0500

    Added dungeon background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Question from "./components/Question";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import Navbar from "./components/Navbar";
 
 function App() {
+ 
   return (
-    <Router>
-      <div>
-        <Navbar />
+
     <Container>
-      
       <Card>
-      <Question />
+        <Question />
       </Card>
-      </Container>
-      </div>
-      </Router>
+    </Container>
+
+
   );
 }
 
