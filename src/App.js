96e301be71a57f79381db32ba4417a54779bb0d6
Jonathan Smith <jonathansmith@Jonathans-MacBook-Pro-2.local>
2021-03-02 12:40:57 -0500

    created some components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import Question from "./components/Question";
+import Container from "./components/Container";
+import Card from "./components/Card";
 
 function App() {
   return (
+    <Container>
+      <Card>
       <Question />
+      </Card>
+      </Container>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import CardBtn from "../CardBtn";
+import "./style.css";
+import Question from "../Question";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <Question />
+    </div>
+  );
+}
+
+export default Card;
