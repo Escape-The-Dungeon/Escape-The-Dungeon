46d21752013709ae6015e77384f8a716eb184615
Leandro Parrado <lparrado321@gmail.com>
2021-03-03 22:00:50 -0500

    Created title.css and linked to title.js

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -1,22 +1,23 @@
 import React from "react";
 import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
-import Card from "../components/Card";
+import { Link } from "react-router-dom";
+import "./title.css";
+
 
 function Title() {
     return(
         <Container>
-            <Row>
-                <Col size="md-4">
-                    <h1>Welcome to Escape-The-Dungeon!</h1>
-                    <Card />
-                </Col>
-                <Col size="md-2">
-                    <h3>What will you do?</h3>
-                <img className="img-thumbnail" src="./images/Weapon.jpeg" width="50" height="50" alt="weapon" />
-                </Col>
-            </Row>
+            <section className="titleContainer">
+            <h1>Welcome to Escape-The-Dungeon!</h1>
+            <h3>What will you do?</h3>
+            <img className="img-thumbnail" src="./images/Weapon.jpeg" width="50" height="50" alt="weapon" />
+            <Link
+              to="/game"
+              className={window.location.pathname === "/game" ? "nav-link active" : "nav-link"}
+            >
+              Start Game
+            </Link>
+            </section>
         </Container>
     )
 }
