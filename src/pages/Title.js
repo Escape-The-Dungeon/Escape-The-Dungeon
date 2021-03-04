747898502e039e238bb7017d79e7b1ac58e013dd
Leandro Parrado <lparrado321@gmail.com>
2021-03-03 22:34:10 -0500

    Stylized title page

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -10,7 +10,7 @@ function Title() {
             <section className="titleContainer">
             <h1>Welcome to Escape-The-Dungeon!</h1>
             <h3>What will you do?</h3>
-            <img className="img-thumbnail" src="./images/Weapon.jpeg" width="50" height="50" alt="weapon" />
+            <img className="img-thumbnail" src="./images/background.png" alt="weapon" />
             <Link
               to="/game"
               className={window.location.pathname === "/game" ? "nav-link active" : "nav-link"}
