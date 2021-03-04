c3b140843fec328d049d58dbeb1aa90500eb0292
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-03-04 15:55:25 -0500

    game is functional

--- src/components/Textbox/story.js
+++ src/components/Textbox/story.js
@@ -20,7 +20,7 @@ let story =  [
             "options": [
                 {
                     "name": "keep going",
-                    "next": 6
+                    "next": 5
                 },
                 {
                     "name": "turn back",
@@ -65,72 +65,166 @@ let story =  [
             "options": [
                 {
                     "name": "go straight",
-                    "next": "7"
+                    "next": 6
                 },
                 {
                     "name": "go left",
-                    "next": "8"
+                    "next": 7
                 }
             ]
 
         },
         
-//         {
-//             "id": 6,
-//             "blob": "You pass by the monster, your heart beating faster with each step but he pays you no mind. He seems to be infused to the wall. You keep down the hallway and notice a door to your left. Do you ",
-//             "options": [
-//                 {
-//                     "name": "go straight",
-//                     "next": "9"
-//                 },
-//                 {
-//                     "name": "go left",
-//                     "next": "10"
-//                 }
-//             ]
+        {
+            "id": 5,
+            "blob": "You pass by the monster, your heart beating faster with each step but he pays you no mind. He seems to be infused to the wall. You keep down the hallway and notice a door to your left. Do you ",
+            "options": [
+                {
+                    "name": "go straight",
+                    "next": 8
+                },
+                {
+                    "name": "go left",
+                    "next": 9
+                }
+            ]
 
-//         },
-//         {
-//             "id": 7,
-//             "blob": "You continue down the expansive hallway, you see sudden movement from up above. Bats? I must be getting closer to the exit you think. You keep walking and see some light up ahead, torch light. You walk upon a dimly lit room with a hospital bed in the far left corner, medical equipment strewn about and on a desk in the middle. A man sits in a chair, scribbling into a notebook. He’s dressed as if a doctor, wearing a mask with a long sharp nose. “Come in”, he says “I can smell your infection from here” His hand snaps up from behind the book and touches your arm. Immediately your dead, your body becoming one of the doctors frightful creations. THE END",
-//             "options": [
-//                 {
-//                     "name": "Start over",
-//                     "next": "1"
-//                 },
-//             ]
+        },
+        {
+            "id": 6,
+            "blob": "You continue down the expansive hallway, you see sudden movement from up above. Bats? I must be getting closer to the exit you think. You keep walking and see some light up ahead, torch light. You walk upon a dimly lit room with a hospital bed in the far left corner, medical equipment strewn about and on a desk in the middle. A man sits in a chair, scribbling into a notebook. He’s dressed as if a doctor, wearing a mask with a long sharp nose. “Come in”, he says “I can smell your infection from here” His hand snaps up from behind the book and touches your arm. Immediately your dead, your body becoming one of the doctors frightful creations. THE END",
+            "options": [
+                {
+                    "name": "Start over",
+                    "next": 0
+                },
+                {
+                    "name": "Start over",
+                    "next": 0
+                },
+            ]
 
-//         },
-//         {
-//             "id": 8,
-//             "blob": "You walk down a few flights of stairs and start hearing running water. You enter a dark room, still unable to see the source of the sound, the rest of the room falls into darkness but theres a metal door to your right. Do you",
-//             "options": [
-//                 {
-//                     "name": "explore room",
-//                     "next": "11"
-//                 },
-//                 {
-//                     "name": "open metal door",
-//                     "next": "12"
-//                 }
-//             ]
+        },
+        {
+            "id": 7,
+            "blob": "You walk down a few flights of stairs and start hearing running water. You enter a dark room, still unable to see the source of the sound, the rest of the room falls into darkness but theres a metal door to your right. Do you",
+            "options": [
+                {
+                    "name": "explore room",
+                    "next": 10
+                },
+                {
+                    "name": "open metal door",
+                    "next": 11
+                }
+            ]
 
-//         },
-//         {
-//             "id": 9,
-//             "blob": "You continue down a hallway that is widening out and seems to be going uphill. You grow hopeful and then you suddenly come across a fork in the road. You look into the left path and notice the stone walls turn into dirt. You look into the left path and notice no changeWhich path calls out to you?",
-//             "options": [
-//                 {
-//                     "name": "dirt-walled tunnel",
-//                     "next": "13"
-//                 },
-//                 {
-//                     "name": "regular tunnel",
-//                     "next": "14"
-//                 }
-//             ]
+        },
+        {
+            "id": 8,
+            "blob": "You continue down a hallway that is widening out and seems to be going uphill. You grow hopeful and then you suddenly come across a fork in the road. You look into the left path and notice the stone walls turn into dirt. You look into the left path and notice no changeWhich path calls out to you?",
+            "options": [
+                {
+                    "name": "dirt-walled tunnel",
+                    "next": 12
+                },
+                {
+                    "name": "regular tunnel",
+                    "next": 13
+                }
+            ]
 
-//         },
+        },
+        {
+            "id": 9,
+            "blob": "You stumble down a few steps and come into a dimly lit room. You see something crouched down in the middle of the barren floor, they seem to be weeping. Is this a trap? You start heading out the way you came in but before you can fully turn around the THING in the room springs up and breaks your neck. THE END ",
+            "options": [
+                {
+                    "name": "start over",
+                    "next": 0
+                },
+                {
+                    "name": "start over",
+                    "next": 0
+                }
+            ]
+
+        },
+        {
+            "id": 10,
+            "blob": "You walk further into the room and suddenly fall head first into rushing water. The pull is too strong and you are unable to pull yourself out. Before you know it you’re pulled deeper underground and you drown. THE END",
+            "options": [
+                {
+                    "name": "start over",
+                    "next": 0
+                },
+                {
+                    "name": "start over",
+                    "next": 0
+                }
+            ]
+
+        },
+        {
+            "id": 11,
+            "blob": "You enter a hallway and you seem to be going uphill, you start feeling a little hopeful. Finally after what feels like ages you reach the end of the hallway. You have the ability to go straight into another room or make a right down another hallway. Which do you choose?",
+            "options": [
+                {
+                    "name": "enter room",
+                    "next": 14
+                },
+                {
+                    "name": "go right",
+                    "next": 8
+                }
+            ]
+
+        },
+        {
+            "id": 12,
+            "blob": "You begin walking down the hallway, unsure of your choice. You start thinking of turning back but then you feel a slight breeze, is that light? You start running and break free of your underground prison.. Only to find your world is now at war. YOU WIN",
+            "options": [
+                {
+                    "name": "start over",
+                    "next": 0
+                },
+                {
+                    "name": "start over",
+                    "next": 0
+                }
+            ]
+
+        },
+        {
+            "id": 13,
+            "blob": "You pick the right tunnel and give it up to fate, you’ve made it this far, right? The tunnel continues uphill and you grow increasingly more hopefully but little by little the tunnel grows narrower till you’re at the point where you have to push yourself through. But wait, is that light up ahead? You push yourself further and further into the narrow cave until you can’t move anymore. But that light, its just around the corner.. You think there must be another way out so you try to turn back but you cant move. You’ve wedged yourself so deep into the cave and now youre stuck. This was a trap, you think to yourself as you wait out your long inevitable death. THE END",
+            "options": [
+                {
+                    "name": "start over",
+                    "next": 0
+                },
+                {
+                    "name": "start over",
+                    "next": 0
+                }
+            ]
+
+        },
+        {
+            "id": 14,
+            "blob": "You stumble down a few steps and come into a dimly lit room. You see something crouched down in the middle of the barren floor, they seem to be weeping. Is this a trap? You start heading out the way you came in but before you can fully turn around the THING in the room springs up and breaks your neck. THE END",
+            "options": [
+                {
+                    "name": "start over",
+                    "next": 0
+                },
+                {
+                    "name": "start over",
+                    "next": 0
+                }
+            ]
+
+        },
 
    ]
  
