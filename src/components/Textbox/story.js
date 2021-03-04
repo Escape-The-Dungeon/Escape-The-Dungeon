b1c93a7de95f1fca0e8f6436379f21e0555f8af2
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-03-04 14:50:38 -0500

    questions go in order

--- src/components/Textbox/story.js
+++ src/components/Textbox/story.js
@@ -1,18 +1,30 @@
-export default {
-
-
-    "story": [
+let story =  [
         {
-            "id": 1,
+            "id": 0,
             "blob": "You wake up in a cellar, the only light being a torch on the wall showing a single open doorway in front of you. The walls to your left and right are stone and barren. You take the torch off the wall and immediately walk through the doorway, you never liked being underground and now youre starting to feel claustrophobic. On the other side of the rounded doorway you find yourself in a tiny room with an opening to your left and right. Which do you feel most compelled to take?",
             "options": [
                 {
                     "name": "right",
-                    "next": "2"
+                    "next": 2
                 },
                 {
                     "name": "left",
-                    "next": "3"
+                    "next": 1
+                }
+            ]
+
+        },
+        {
+            "id": 1,
+            "blob": "You start going down a damp hallway and become heavily uneasy because of the giant lurking in the corner and his eyes inpentreting you like daggers. Do you:",
+            "options": [
+                {
+                    "name": "keep going",
+                    "next": 6
+                },
+                {
+                    "name": "turn back",
+                    "next": 2
                 }
             ]
 
@@ -23,29 +35,30 @@ export default {
             "options": [
                 {
                     "name": "left",
-                    "next": "4"
+                    "next": 4
                 },
                 {
                     "name": "straight",
-                    "next": "5"
+                    "next": 3
                 }
             ]
         },
         {
             "id": 3,
-            "blob": "You start going down a damp hallway and become heavily uneasy because of the giant lurking in the corner and his eyes inpentreting you like daggers. Do you:",
+            "blob": "You open the door, its just an empty room. You take a few steps in and suddenly you’re falling.. Falling falling, down an endless pit. THE END ",
             "options": [
                 {
-                    "name": "keep going",
-                    "next": "6"
+                    "name": "Start over",
+                    "next": 0
                 },
                 {
-                    "name": "turn back",
-                    "next": "2"
-                }
+                    "name": "Start over",
+                    "next": 0
+                },
             ]
 
         },
+       
         {
             "id": 4,
             "blob": "You enter the hallway, its pretty big in here, you think to yourself. You can’t even see the ceiling. You notice an entry on your left with stairs leading down into darkness.",
@@ -61,73 +74,66 @@ export default {
             ]
 
         },
-        {
-            "id": 5,
-            "blob": "You open the door, its just an empty room. You take a few steps in and suddenly you’re falling.. Falling falling, down an endless pit. THE END ",
-            "options": [
-                {
-                    "name": "Start over",
-                    "next": "1"
-                },
-            ]
+        
+//         {
+//             "id": 6,
+//             "blob": "You pass by the monster, your heart beating faster with each step but he pays you no mind. He seems to be infused to the wall. You keep down the hallway and notice a door to your left. Do you ",
+//             "options": [
+//                 {
+//                     "name": "go straight",
+//                     "next": "9"
+//                 },
+//                 {
+//                     "name": "go left",
+//                     "next": "10"
+//                 }
+//             ]
 
-        },
-        {
-            "id": 6,
-            "blob": "You pass by the monster, your heart beating faster with each step but he pays you no mind. He seems to be infused to the wall. You keep down the hallway and notice a door to your left. Do you ",
-            "options": [
-                {
-                    "name": "go straight",
-                    "next": "9"
-                },
-                {
-                    "name": "go left",
-                    "next": "10"
-                }
-            ]
+//         },
+//         {
+//             "id": 7,
+//             "blob": "You continue down the expansive hallway, you see sudden movement from up above. Bats? I must be getting closer to the exit you think. You keep walking and see some light up ahead, torch light. You walk upon a dimly lit room with a hospital bed in the far left corner, medical equipment strewn about and on a desk in the middle. A man sits in a chair, scribbling into a notebook. He’s dressed as if a doctor, wearing a mask with a long sharp nose. “Come in”, he says “I can smell your infection from here” His hand snaps up from behind the book and touches your arm. Immediately your dead, your body becoming one of the doctors frightful creations. THE END",
+//             "options": [
+//                 {
+//                     "name": "Start over",
+//                     "next": "1"
+//                 },
+//             ]
 
-        },
-        {
-            "id": 7,
-            "blob": "You continue down the expansive hallway, you see sudden movement from up above. Bats? I must be getting closer to the exit you think. You keep walking and see some light up ahead, torch light. You walk upon a dimly lit room with a hospital bed in the far left corner, medical equipment strewn about and on a desk in the middle. A man sits in a chair, scribbling into a notebook. He’s dressed as if a doctor, wearing a mask with a long sharp nose. “Come in”, he says “I can smell your infection from here” His hand snaps up from behind the book and touches your arm. Immediately your dead, your body becoming one of the doctors frightful creations. THE END",
-            "options": [
-                {
-                    "name": "Start over",
-                    "next": "1"
-                },
-            ]
+//         },
+//         {
+//             "id": 8,
+//             "blob": "You walk down a few flights of stairs and start hearing running water. You enter a dark room, still unable to see the source of the sound, the rest of the room falls into darkness but theres a metal door to your right. Do you",
+//             "options": [
+//                 {
+//                     "name": "explore room",
+//                     "next": "11"
+//                 },
+//                 {
+//                     "name": "open metal door",
+//                     "next": "12"
+//                 }
+//             ]
 
-        },
-        {
-            "id": 8,
-            "blob": "You walk down a few flights of stairs and start hearing running water. You enter a dark room, still unable to see the source of the sound, the rest of the room falls into darkness but theres a metal door to your right. Do you",
-            "options": [
-                {
-                    "name": "explore room",
-                    "next": "11"
-                },
-                {
-                    "name": "open metal door",
-                    "next": "12"
-                }
-            ]
+//         },
+//         {
+//             "id": 9,
+//             "blob": "You continue down a hallway that is widening out and seems to be going uphill. You grow hopeful and then you suddenly come across a fork in the road. You look into the left path and notice the stone walls turn into dirt. You look into the left path and notice no changeWhich path calls out to you?",
+//             "options": [
+//                 {
+//                     "name": "dirt-walled tunnel",
+//                     "next": "13"
+//                 },
+//                 {
+//                     "name": "regular tunnel",
+//                     "next": "14"
+//                 }
+//             ]
 
-        },
-        {
-            "id": 9,
-            "blob": "You continue down a hallway that is widening out and seems to be going uphill. You grow hopeful and then you suddenly come across a fork in the road. You look into the left path and notice the stone walls turn into dirt. You look into the left path and notice no changeWhich path calls out to you?",
-            "options": [
-                {
-                    "name": "dirt-walled tunnel",
-                    "next": "13"
-                },
-                {
-                    "name": "regular tunnel",
-                    "next": "14"
-                }
-            ]
+//         },
 
-        },
+   ]
+ 
 
-    ]
-}
+
+ module.exports = story;
