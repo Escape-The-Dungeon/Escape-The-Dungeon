fa0434f45b115e171b923fb2f2114ea2ea4a9988
Leandro Parrado <lparrado321@gmail.com>
2021-03-09 11:13:32 -0500

    Add media queries in css

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -9,29 +9,29 @@ import Col from "../components/Col";
 
 
 function Game() {
-    return (
+  return (
     <Container>
       <Navbar />
       <Col>
-      <Row>
-      <Torches />
-      </Row>
+        <Row>
+          <Torches />
+        </Row>
       </Col>
       <Col>
-      <Row>
-      <Card backgroundImage="https://cdn.pixabay.com/photo/2015/11/08/05/58/scroll-1033117_960_720.jpg">
-       
-        <Textbox />
-      </Card>
-      </Row>
+        <Row>
+          <Card backgroundImage="https://cdn.pixabay.com/photo/2015/11/08/05/58/scroll-1033117_960_720.jpg">
+
+            <Textbox />
+          </Card>
+        </Row>
       </Col>
       <Col>
-      <Row>
-      <Torches />
-      </Row>
+        <Row>
+          <Torches />
+        </Row>
       </Col>
     </Container>
-    )
+  )
 }
 
 export default Game;
