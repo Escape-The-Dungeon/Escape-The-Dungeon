d72a89cf37a9e0b85c6ea9270df2f5af403e3633
Jonathan Smith <jpls218@gmail.com>
2021-03-09 11:14:43 -0500

    fixed torches positions

--- client/src/pages/Youtube.js
+++ client/src/pages/Youtube.js
@@ -4,7 +4,8 @@ import VideoDetail from "../components/VideoDetail";
 import VideoList from "../components/VideoList";
 import Card from "../components/Card";
 import Torches from "../components/Torches";
-import Row from "../components/Row";
+import "./torch.css";
+
 
 class Youtube extends React.Component {
     state = {
@@ -19,10 +20,10 @@ class Youtube extends React.Component {
 
         return (
            
+           
             <Card backgroundImage="https://cdn.pixabay.com/photo/2015/11/08/05/58/scroll-1033117_960_720.jpg">
-         <h1> Recomended videos </h1>
-         <Row>
-             <Torches />
+                <h1> Recomended videos </h1>
+        
                 <div className='ui grid'>
                     <div className='ui grid'>
                         <div className='ui row'>
@@ -36,9 +37,11 @@ class Youtube extends React.Component {
                         </div>
                     </div>
                 </div>
-                <Torches />
-            </Row>
+        
             </Card>
+            
+            
+           
         )
     }
 }
