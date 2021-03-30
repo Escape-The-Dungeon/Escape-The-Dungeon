165ba42ef655e9c91e3b1d5f55453541a86c136f
Leandro Parrado <lparrado321@gmail.com>
2021-03-30 10:56:46 -0400

    Modified About Us page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -33,32 +33,32 @@ function About() {
           <p>Creators of the game:</p>
           <p>--------------------------------</p>
           <Row>
-          <img src="images/pic.JPG" alt="Placeholder" className="profile col-md-4"></img>
-          <p className="text-wrap col-md-6">Ashlyn Hicks is a hardworking coder who loves videogames. Her vivid imagination led to the creation of a large portion of this game's story.</p>
+            <img src="images/pic.JPG" alt="Placeholder" className="profile col-md-4"></img>
+            <p className="text-wrap col-md-6">Ashlyn Hicks is a hardworking coder who loves videogames. Her vivid imagination led to the creation of a large portion of this game's story.</p>
           </Row>
           <a href="https://github.com/jpls218" className="fa fa-github"></a>
           <a href="https://www.linkedin.com/in/jonathan-smith-9498781ba/" class="fa fa-linkedin"></a>
           <br></br>
-        
+
 
           {/* Picture */}
-         
-<Row>
+
+          <Row>
             <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/File_000.jpeg" alt="Placeholder" className="profile col-md-4" />
-            
-    
-            
-            <p className="text-wrap col-md-6">Leandro Parrado is the coder/artist who created all of the illustrations seen in this game. He also helped write some of the story seen in the game.</p>
-            </Row>
+
+
+
+            <p className="text-wrap col-md-6">Leandro Parrado is the coder/artist who created all of the illustrations seen in the game and also co-wrote the story. </p>
+          </Row>
           <a href="https://github.com/Bokosyk" className="fa fa-github"></a>
-          
+
           <a href="https://www.linkedin.com/in/leandro-parrado-a05729166/" class="fa fa-linkedin"></a>
           <br></br>
-          
-          
+
+
           <Row>
-          <img src="images/jon.jpeg" alt="Placeholder" className="profile col-md-4"></img>
-          <p className="text-wrap col-md-6">Jonathan Smith is a coder who helped edit and contribute to the overall style and design of the game.</p>
+            <img src="images/IMG_3610.jpeg" alt="Placeholder" className="profile col-md-4"></img>
+            <p className="text-wrap col-md-6">Jonathan Smith is a coder who helped edit and contribute to the overall style and design of the game.</p>
           </Row>
           <a href="https://github.com/jpls218" className="fa fa-github"></a>
           <a href="https://www.linkedin.com/in/jonathan-smith-9498781ba/" class="fa fa-linkedin"></a>
