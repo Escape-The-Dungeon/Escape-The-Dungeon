2e9c53cb6f77419601db9e2ea7d4c0bd0fce303e
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-03-29 21:48:47 -0400

    Revert "Merge pull request #102 from Escape-The-Dungeon/gamedatabase"
    This reverts commit f2c6ed225eca91188bfe4f7efe04e1bb472957b6, reversing
    changes made to c8535dadb31111b20afef4d046e1b0a63c0dc9db.

--- routes/api/story.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const storyController = require("../../controllers/storyController");
-
-// Matches with "/api"
-router.route("/")
-    .get(storyController.findAll)
-
-module.exports = router;
